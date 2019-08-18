9817a04588f5fda912556b80f2db5c824f1d7ec7
xudongxue <1229594763@qq.com>
2019-08-18 23:36:39 +0800

    feat: 完成对vcxproject添加qt特性(file、director)

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,60 @@
 #include <boost/regex.hpp>
 
 namespace po = boost::program_options;
+std::string gQtVersion;
+std::string gQtGeneratedDir;
 
 void AddQt2Vs(const std::string &filePath)
 {
+	using namespace pugi;
 
+	xml_document doc;
+	if (!doc.load_file(filePath.c_str(), pugi::parse_default, pugi::encoding_utf8))
+	{
+		std::cout << "load file '" << filePath.c_str() << "' failed."<< std::endl;
+		return ;
+	}
+
+	xml_node projNode = doc.child("Project");
+	if (!projNode)
+	{
+		return;
+	}
+
+	xpath_node globalsPropNode = doc.select_single_node("/Project/PropertyGroup[@Label='Globals']");
+	if (!globalsPropNode)
+	{
+		std::cout << "file: " << filePath.c_str() << "add qt to vs failed, detail: /Project/PropertyGroup/[@Label = 'Globals'] not exit!"
+			<< std::endl;
+		return;
+	}
+	
+	globalsPropNode.node().remove_child("Keyword");
+	globalsPropNode.node().append_child("Keyword").append_child(pugi::node_pcdata).set_value("Qt4VSv1.0");
+
+	xml_node projExtNode = projNode.child("ProjectExtensions");
+	if (!projExtNode)
+	{
+		projExtNode = projNode.append_child("ProjectExtensions");
+	}
+
+	xml_node projExtVsNode = projExtNode.child("VisualStudio");
+	if (!projExtVsNode)
+	{
+		projExtVsNode = projExtNode.append_child("VisualStudio");
+	}
+
+ 	projExtVsNode.remove_child("UserProperties");
+	xml_node projExtVsUserPropNode = projExtVsNode.append_child("UserProperties");
+	
+	projExtVsUserPropNode.append_attribute("UicDir").set_value(gQtGeneratedDir.c_str());
+	projExtVsUserPropNode.append_attribute("MocDir").set_value(gQtGeneratedDir.c_str());
+	projExtVsUserPropNode.append_attribute("RccDir").set_value(gQtGeneratedDir.c_str());
+
+	projExtVsUserPropNode.append_attribute("Qt5Version_x0020_Win32").set_value(gQtVersion.c_str());
+	projExtVsUserPropNode.append_attribute("Qt5Version_x0020_x64").set_value(gQtVersion.c_str());
+
+	doc.save_file(filePath.c_str());
 }
 
 bool ConvertVcxproj2Qt(const std::string &pathStr)
@@ -22,7 +72,7 @@ bool ConvertVcxproj2Qt(const std::string &pathStr)
 		{
 			ConvertVcxproj2Qt(pathIt->path().string());
 		}
-		else if (boost::regex_match(pathIt->path().filename().string(), boost::regex("*.vcxproj")))
+		else if (boost::regex_match(pathIt->path().filename().string().c_str(), boost::regex(".*.vcxproj")))
 		{
 			AddQt2Vs(pathIt->path().string());
 		}
@@ -33,22 +83,27 @@ bool ConvertVcxproj2Qt(const std::string &pathStr)
 
 int main(int argc, char **argv)
 {
-	boost::program_options::options_description opts("demo options");
+	po::options_description opts("support options");
 	opts.add_options()
 		("help,h", "show help")
-		("file,f", boost::program_options::value<std::string>(), "path of vcxproj file")
-		("directory,d", boost::program_options::value<std::string>(), "path of vcxproj files directory");
-	
-	boost::program_options::variables_map valueMap;
-	boost::program_options::store(boost::program_options::parse_command_line(argc, argv, opts), valueMap);
+		("file,f", po::value<std::string>(), "path of vcxproj file")
+		("directory,d", po::value<std::string>(), "path of vcxproj files directory")
+		("version,v", po::value<std::string>()->default_value("5.10.0"), "qt version")
+		("generatedDir,g", po::value<std::string>()->default_value("./GeneratedFiles/$(ProjectName)/$(ConfigurationName)"), "qt moc director");
+
+	po::variables_map valueMap;
+	po::store(po::parse_command_line(argc, argv, opts), valueMap);
 
 	if (valueMap.size() == 0 || valueMap.count("help")){
 		std::cout << opts << std::endl;
 		return 0;
 	}
 
+	gQtVersion = valueMap["version"].as<std::string>();
+	gQtGeneratedDir = valueMap["generatedDir"].as<std::string>();
+
 	if (valueMap.count("file")){
-		ConvertVcxproj2Qt(valueMap["filename"].as<std::string>());
+		AddQt2Vs(valueMap["file"].as<std::string>());
 	}
 	else if (valueMap.count("directory")){
 		ConvertVcxproj2Qt(valueMap["directory"].as<std::string>());
